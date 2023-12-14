87dd1ef0863667d84ffd3160a5208fa228ea36f3
Benoit Quette <benoit.quette@gmail.com>
2023-12-14 16:46:55 +0100

    try and fix the missing schema error

--- models/Results.js
+++ models/Results.js
@@ -4,15 +4,15 @@
  * Module dependencies
  */
 
-var mongoose = require('mongoose');
+import {Schema, model} from 'mongoose';
 
 // end module dependencies
 
-var ResultSchema = new mongoose.Schema({
+var ResultSchema = new Schema({
   timestamp: Date,
   category: String,
   url: String,
-  project: {type: mongoose.Schema.Types.ObjectId, ref: 'Project'},
+  project: {type: Schema.Types.ObjectId, ref: 'Project'},
   rules: [{
     rule: String,
     title: String,
@@ -24,5 +24,5 @@ var ResultSchema = new mongoose.Schema({
   }]
 });
 
-mongoose.model('Result', ResultSchema);
+model('Result', ResultSchema);
 
