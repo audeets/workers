d31a47758ff86db80b79d29f7015700f23e6f8d5
Benoit Quette <benoit.quette@gmail.com>
2024-02-12 10:18:57 +0100

    introduce config string from ENV for puppeteer

--- test/fetcher/fetcher-Spec.js
+++ test/fetcher/fetcher-Spec.js
@@ -10,7 +10,7 @@ const port = "9615";
 const invalidURl = "sdsdsqdsqdsqd.html";
 const staticFile = "www.google.com.html";
 const timeout = 10000;
-const pause = 3000;
+const pause = 4000;
 
 describe("fetcher", function () {
   describe("#fetch()", function () {
@@ -31,10 +31,10 @@ describe("fetcher", function () {
       const spy = sinon.spy();
       fetcher.fetch(url, spy);
       setTimeout(() => {
+        done();
         expect(spy.called).to.equal(true);
         expect(spy.args[0][0]).to.equal(null);
         expect(spy.args[0][1]).to.have.string("Cannot");
-        done();
       }, pause);
     });
     it("should return a static content", function (done) {
