e44406426a9c5244e3595beee58a00e441293293
Benoit Quette <benoit.quette@gmail.com>
2024-02-29 11:18:36 +0100

    quick script to recreate the index from mongo

---
+++ bin/recreate-index.js
@@ -0,0 +1,29 @@
+import log from "./../lib/logger.js";
+import mongoose from "@benoitquette/audeets-api-commons/models/index.js";
+import { process as processResult } from "./../lib/store-elasticsearch/index.js";
+
+mongoose
+  .model("Result")
+  .find()
+  .then((results) => {
+    results.forEach((result) => {
+      const resultCopy = {
+        timestamp: result.timestamp,
+        category: result.category,
+        url: result.url,
+        project: result.project,
+        rules: result.rules.map((r) => ({
+          rule: r.rule,
+          title: r.title,
+          check: r.check,
+          details: r.details,
+        })),
+      };
+      processResult(process.env, resultCopy, (err) => {
+        if (err) log.error(err);
+      });
+    });
+  })
+  .catch((error) => {
+    log.error(error);
+  });
